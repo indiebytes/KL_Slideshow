5e1ebde77f52e6a159e9a824e3d535b34127034d
tykhon <tykhon@karlssonlord.com>
2014-08-28 13:26:48 +0200

    Added getFirstSlide() to slideshow model

--- app/code/community/KL/Slideshow/Model/Slideshow.php
+++ app/code/community/KL/Slideshow/Model/Slideshow.php
@@ -32,4 +32,28 @@ class KL_Slideshow_Model_Slideshow extends Mage_Core_Model_Abstract
     {
         $this->_init('slideshow/slideshow');
     }
+
+    /**
+     * Gets the first item from the current Slideshow. Current store is is used for filtering.
+     *
+     * @return KL_Slideshow_Model_Slide
+     */
+    public function getFirstSlide() {
+        $model = Mage::getModel('slideshow/slide')->getCollection();
+
+        $model->getSelect()
+            ->join(
+                array('slideshow_slide' => $model->getTable('slideshow/slideshow_slide')),
+                'main_table.slide_id = slideshow_slide.slide_id',
+                array('slideshow_slide.*')
+            )
+            ->where('slideshow_slide.slideshow_id = ?', $this->getId());
+
+        return $model->addStoreFilter(Mage::app()->getStore()->getId())
+            ->addFilter('is_active', array('eq' => '1'))
+            ->setOrder('position', 'ASC')
+            ->setPageSize(1)
+            ->load()
+            ->getFirstItem();
+    }
 }
